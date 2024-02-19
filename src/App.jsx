65726027ed5903aea3677a4f573b8d98b9b3cd43
DraxusTDev <draxustienda@gmail.com>
2024-02-19 17:53:19 -0500

    añadiendo estilos

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,12 @@
+import React from "react";
 import Nav from "./components/Nav";
 
 const App = () => {
-  <Nav />;
+  return (
+    <div>
+      <Nav />
+    </div>
+  );
 };
 
 export default App;
--- src/components/Nav.jsx
+++ src/components/Nav.jsx
@@ -1,5 +1,15 @@
 const Nav = () => {
-  <h1>Nav</h1>;
+  const styles = {
+    width: "100%",
+    height: "70px",
+    backgroundColor: "black",
+    color: "white",
+  };
+  return (
+    <div>
+      <nav style={styles}>Nav</nav>
+    </div>
+  );
 };
 
 export default Nav;
